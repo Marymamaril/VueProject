212b2fd862d8cac07427915c43dcd1b2909c46de
Marymamaril <112803306+Marymamaril@users.noreply.github.com>
2022-09-13 13:42:42 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
    <v-app>
+      
    <v-main>
+      <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
       <Navbar/>
       <router-view></router-view>
+      </v-parallax>
    </v-main>
+
    </v-app>
 </template>
 
