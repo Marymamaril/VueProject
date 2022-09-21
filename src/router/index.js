753cc8daac8ac37a657490f2e56639805abe03b7
Marymamaril <112803306+Marymamaril@users.noreply.github.com>
2022-09-21 19:12:45 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ import Aboutme from '../views/Aboutme.vue'
 import vuetify from '../views/Vuetify.vue'
 import AxiosDemo from '../views/AxiosDemo.vue'
 import AboutApp from '../views/AboutApp.vue'
-
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,38 +15,76 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta:
+      {
+        requiresAuth: true
+      }
     },
     {
       path: '/stringApp',
       name: 'StringAppView',
-      component: StringAppView
+      component: StringAppView,
+      meta:
+      {
+        requiresAuth: true
+      }
     },
     {
     path: '/basicMath',
     name: 'basicMath',
-    component: BasicMathView
+    component: BasicMathView,
+    meta:
+    {
+      requiresAuth: true
+    }
     },
     {
       path: '/vuetify',
       name: 'vuetify',
-      component: vuetify
+      component: vuetify,
+      meta:
+      {
+        requiresAuth: true
+      }
       },
     {
       path: '/aboutme',
       name: 'aboutme',
-      component: Aboutme
+      component: Aboutme,
+      meta:
+      {
+        requiresAuth: true
+      }
       },
       {
         path: '/quizapp',
         name: 'quizapp',
-        component: AxiosDemo
+        component: AxiosDemo,
+        meta:
+        {
+          requiresAuth: true
+        }
         },
         {
           path: '/aboutapp',
           name: 'aboutapp',
-          component: AboutApp
+          component: AboutApp,
+          meta:
+          {
+            requiresAuth: true
+          }
           },
+          {
+            path: '/login',
+            name: 'login',
+            component: Login
+            },
+            {
+              path: '/register',
+              name: 'register',
+              component: Register
+              },
   ]
 })
 
